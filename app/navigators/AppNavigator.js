fbcc405c9a93fb49e77cec4c78ddc1bee7613210
zhiying.zhou <zhiying.zhou@sao.so>
2017-09-01 10:46:26 +0800

    perfect AppNavigator

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -3,49 +3,67 @@ import { Image } from 'react-native';
 import { connect } from 'react-redux';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 
-import ShopMallView from '../views/shopmall';
-import ProfileView from '../views/profile';
+import ShopMallView from '../views/shopmall'; // 商城页面
+import ProfileView from '../views/profile'; // 我的页面
 
-import TrainPage from '../pages/train';
-import FlightPage from '../pages/flight';
-import BusPage from '../pages/bus';
-import CityPage from '../pages/city';
-import CalendarPage from '../pages/calendar';
+import TrainPage from '../pages/train'; // 火车票 
+import FlightPage from '../pages/flight'; // 机票
+import BusPage from '../pages/bus'; // 汽车/船票
+import CityPage from '../pages/city'; // 选择城市
+import CalendarPage from '../pages/calendar'; // 选择日历
 
-const HomeNavigator = TabNavigator({
-    '火车票': { screen: TrainPage },
-    '机票': { screen: FlightPage },
-    '汽车/船票': { screen: BusPage },
-}, { 
-    tabBarPosition: 'top',
-    lazyLoad: false,
-    lazy: true,
-    scrollEnabled: true,
-    swipeEnabled: false,
-    animationEnabled: false,
-    tabBarOptions: {
-        // 是否显示icon
-        showIcon: false,
-        // 底部标签栏样式
-        style: {
-            height: 50,
-            backgroundColor: '#FFF'
+const HomeTabNavigator = TabNavigator(
+    {
+        Train: {
+            screen: TrainPage,
+            navigationOptions: {
+                tabBarLabel: '火车票'
+            }
         },
-        tabStyle: {
+        Flight: {
+            screen: FlightPage,
+            navigationOptions: {
+                tabBarLabel: '机票'
+            }
         },
-        labelStyle: {
-            fontSize: 14,
-            color: '#666'
-        },
-        activeTintColor: '#09bb07',
-        // 底部标签栏指示器的样式
-        indicatorStyle: {
-            backgroundColor: '#09bb07'
+        Bus: {
+            screen: BusPage,
+            navigationOptions: {
+                tabBarLabel: '汽车/船票'
+            }
+        }
+    },
+    {
+        tabBarPosition: 'top',
+        lazyLoad: false,
+        lazy: true,
+        scrollEnabled: true,
+        swipeEnabled: false,
+        animationEnabled: false,
+        tabBarOptions: {
+            // 是否显示icon
+            showIcon: false,
+            // 底部标签栏样式
+            style: {
+                height: 50,
+                backgroundColor: '#FFF'
+            },
+            tabStyle: {
+            },
+            labelStyle: {
+                fontSize: 14,
+                color: '#666'
+            },
+            activeTintColor: '#09bb07',
+            // 底部标签栏指示器的样式
+            indicatorStyle: {
+                backgroundColor: '#09bb07'
+            }
         }
     }
-});
+);
 
-const TabNavigatorConfig = {
+const MainTabNavigatorConfig = {
     tabBarPosition: 'bottom',
     swipeEnabled: false,
     animationEnabled: false,
@@ -75,75 +93,77 @@ const TabNavigatorConfig = {
 /**
  * 路由配置
  */
-const obj = {
+const MainNavigatorMapObj = {
     Home: {
-        screen: HomeNavigator,
-        title: '首页',
-        unactiveIcon: require('../images/tab_icon/home.png'),
-        activeIcon: require('../images/tab_icon/home_active.png')
+        screen: HomeTabNavigator,
+        tabBarLabel: '首页',
+        activeIcon: require('../images/tab_icon/home_active.png'),
+        unactiveIcon: require('../images/tab_icon/home.png')
     },
     ShopMall: {
         screen: ShopMallView,
-        title: '商城',
-        unactiveIcon: require('../images/tab_icon/shopmall.png'),
-        activeIcon: require('../images/tab_icon/shopmall_active.png')
+        tabBarLabel: '商城',
+        activeIcon: require('../images/tab_icon/shopmall_active.png'),
+        unactiveIcon: require('../images/tab_icon/shopmall.png')
     },
     Profile: {
         screen: ProfileView,
-        title: '我的',
-        unactiveIcon: require('../images/tab_icon/profile.png'),
-        activeIcon: require('../images/tab_icon/profile_active.png')
+        tabBarLabel: '我的',
+        activeIcon: require('../images/tab_icon/profile_active.png'),
+        unactiveIcon: require('../images/tab_icon/profile.png')
     }
 };
 
-const generateRouteConfig = (route) => ({ 
+const generateRouteConfig = (route) => ({
     screen: route.screen,
     navigationOptions: {
-        title: route.title,
+        tabBarLabel: route.tabBarLabel,
         tabBarIcon({ focused }) {
             return <Image
                 style={{
                     width: 68,
                     height: 68 / 1.125
                 }}
-                source= { focused ? route.activeIcon : route.unactiveIcon }
+                source={focused ? route.activeIcon : route.unactiveIcon}
             />;
         }
     }
 });
 
-const RouteConfigs = {};
+const MainRouteConfigs = {};
 
 [
-    'Home', 
-    'ShopMall', 
+    'Home',
+    'ShopMall',
     'Profile'
 ].forEach((item) => {
-    RouteConfigs[item] = generateRouteConfig(obj[item]);
+    MainRouteConfigs[item] = generateRouteConfig(MainNavigatorMapObj[item]);
 });
 
-export const AppNavigator = TabNavigator(RouteConfigs, TabNavigatorConfig);
+export const MainNavigator = TabNavigator(MainRouteConfigs, MainTabNavigatorConfig);
 
-const MainNavigator = StackNavigator({
-    
-    'main': { screen: AppNavigator, 
+const AppNavigator = StackNavigator({
+    'main': {
+        screen: MainNavigator,
         navigationOptions: {
             header: null
-        } },
-    'city': { screen: CityPage, 
+        }
+    },
+    'city': {
+        screen: CityPage,
         navigationOptions: {
-            title: '选择城市'
-        } },
+            headerTitle: '选择城市'
+        }
+    },
     'calendar': {
         screen: CalendarPage,
         navigationOptions: {
-            title: '选择日期'
+            headerTitle: '选择日期'
         }
     },
-    
-}, {
+
 });
 
-const AppWithNavigationState = () => <MainNavigator />;
+const AppWithNavigationState = () => <AppNavigator />;
 
 export default connect()(AppWithNavigationState);
