641050896f8ea4f39ef6b5ef49a5d61702d61273
zhiyingzzhou <zhiyingzzhou@gmail.com>
2017-09-01 14:52:47 +0800

    modify util's get ajax request

--- app/util/index.js
+++ app/util/index.js
@@ -4,22 +4,21 @@ import { StyleSheet, Platform } from 'react-native';
 import axios from 'axios';
 import lodash from './lodash.custom.min.js';
 
-const Util = () => {};
+const Util = () => { };
 
 Util.prototype = {
     constructor: Util,
-    os: Platform.OS,
     hairlineWidth: StyleSheet.hairlineWidth,
     prefixUri: 'http://wx.17u.cn/'
 };
 
 Util.prototype.debounce = lodash.debounce;
 
-Util.prototype.isNull = function(value) {
+Util.prototype.isNull = function (value) {
     return value === null;
 };
 
-Util.prototype.chunk = function(arr, num) {
+Util.prototype.chunk = function (arr, num) {
     num = Number(num) || 1;
     const ret = [];
 
@@ -29,33 +28,40 @@ Util.prototype.chunk = function(arr, num) {
         }
         ret[ret.length - 1].push(item);
     });
-    
+
     return ret;
 };
 
-Util.prototype.get = async (uri, data) => {
-    try {    
-        return await axios.get(_.prefixUri + uri, {
+Util.prototype.get = (uri, data) => {
+    return new Promise((resolve, reject) => {
+        axios.get(_.prefixUri + uri, {
             params: data
-        });
-    } catch (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
-    }
+        })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+    //     if (error.response) {
+    //         // The request was made and the server responded with a status code
+    //         // that falls out of the range of 2xx
+    //         // console.log(error.response.data);
+    //         // console.log(error.response.status);
+    //         // console.log(error.response.headers);
+    //     } else if (error.request) {
+    //         // The request was made but no response was received
+    //         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    //         // http.ClientRequest in node.js
+    //         // console.log(error.request);
+    //     } else {
+    //         // Something happened in setting up the request that triggered an Error
+    //         // console.log('Error', error.message);
+    //     }
+    //     // console.log(error.config);
+
+    //     return error;
 };
 
 const _ = new Util();
