af1230c29e3b7036c3329585c7e7b27637b33268
zhiyingzzhou <zhiyingzzhou@gmail.com>
2017-09-01 14:59:29 +0800

    add react-native-storage library and config react-native-storage

--- index.android.js
+++ index.android.js
@@ -9,19 +9,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+// 配置react-native-storage
+import './app/configureStorage';
+
 import AppReducer from './app/reducers';
 import AppWithNavigationState from './app/navigators/AppNavigator';
 
 class LyRN extends React.Component {
-  store = createStore(AppReducer, applyMiddleware(thunk));
-  
-  render() {
-      return (
-          <Provider store={this.store}>
-              <AppWithNavigationState />
-          </Provider>
-      );
-  }
+    store = createStore(AppReducer, applyMiddleware(thunk));
+
+    render() {
+        return (
+            <Provider store={this.store}>
+                <AppWithNavigationState />
+            </Provider>
+        );
+    }
 }
 
 AppRegistry.registerComponent('LyRN', () => LyRN);
